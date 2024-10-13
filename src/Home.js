8f985b6a04edb6a8a8acea8bc3f38503a362502e
wendy munyasi <wendymunyasi@gmail.com>
2024-10-13 17:28:26 +0300

    Add useState hooks for blogs; add css

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
-  const Home = () => {
-    return ( 
-      <div className="home">
-        <h2>Homepage</h2>
-      </div>
-    );
-  }
-   
-  export default Home;
+import { useState } from 'react';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Xander', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Wolfe', id: 2 },
+    { title: 'Web dev resources', body: 'lorem ipsum...', author: 'Vali', id: 3 },
+  ]);
+
+  return ( 
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
