4db28a7dafa88b2710e68f1859d1943c4d4eec17
wendy munyasi <wendymunyasi@gmail.com>
2024-10-13 13:26:01 +0300

    Add sample of inline styling, edit index.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
           <a href="/">Home</a>
-          <a href="/create">New Blog</a>
+          <a href="/create" style={{  // inline styling 
+            color: 'white', // style is an object of key-value pairs
+            backgroundColor: '#f1356d', // hence the use of second {}
+            borderRadius: '8px'
+          }} >New Blog</a>
       </div>
     </nav>
   );
