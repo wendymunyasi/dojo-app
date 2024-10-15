3f38d67df130383877d17aff289ad0410ff6cdba
wendy munyasi <wendymunyasi@gmail.com>
2024-10-15 17:14:11 +0300

    Pass a function into a component as a prop

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,9 +1,10 @@
 const BlogList = (props) => {
   const blogs  = props.blogs;
   const title = props.title;
+  const handleDelete = props.handleDelete;
 // you can destructure the props directly in the const
 // and write what properties you want: i.e;
-// const BlogList = ({blogs, title}) => {
+// const BlogList = ({blogs, title, handleDelete}) => {
 // then don't  declare the const variables
 
   return (
@@ -13,6 +14,11 @@ const BlogList = (props) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}.</p>
+          {/* Define a function that will be called when the button is clicked 
+          and pass the blog.id as a parameter
+          Declare the function in Home.js because that is where the state
+          is */}
+          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,19 @@ const Home = () => {
     { title: 'Web dev resources', body: 'lorem ipsum...', author: 'Xander', id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    // This doesn't change the original data but it returns a new filtered
+    // array based on the original data
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Xander')} title="Xander's blogs!" />
+      {/* Create a prop called handleDelete and set it equal to handleDelete function */}
+      {/* pass the props in Home.js */}
+      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Xander')} title="Xander's blogs!" /> */}
     </div>
   );
 }
