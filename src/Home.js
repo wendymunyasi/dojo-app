f7885727952207b3d724df4399c73ac6d899e846
wendy munyasi <wendymunyasi@gmail.com>
2024-10-15 16:55:12 +0300

    Add a way to filter and display logs by author

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'Xander', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Wolfe', id: 2 },
-    { title: 'Web dev resources', body: 'lorem ipsum...', author: 'Vali', id: 3 },
+    { title: 'Web dev resources', body: 'lorem ipsum...', author: 'Xander', id: 3 },
   ]);
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All blogs!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Xander')} title="Xander's blogs!" />
     </div>
   );
 }
