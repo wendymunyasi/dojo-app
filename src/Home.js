700545f40327ead2c45d997b3899bd00ce298df3
wendy munyasi <wendymunyasi@gmail.com>
2024-10-23 20:15:50 +0300

    Comment out old useEffect hook, others

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     { title: 'Web dev resources', body: 'lorem ipsum...', author: 'Xander', id: 3 },
   ]);
 
-  const [name, setName] = useState('Dante');
+  // const [name, setName] = useState('Dante');
 
   const handleDelete = (id) => {
     // This doesn't change the original data but it returns a new filtered
@@ -17,7 +17,7 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
-  useEffect(() => {
+  // useEffect(() => {
     // useEffect hook runs a function every render of the component
     // Used for things like fetching data or communication with a
     // sort of authentication service. Those are called side effects
@@ -25,9 +25,9 @@ const Home = () => {
     // The state is affected and that triggers a rerender which triggers
     // the function in useEffect to run which changes the state again
     // triggering a rerender again etc - LOOP
-    console.log("RUN USE EFFECT");
-    console.log(name);
-  }, [name]);   // [name] is a dependency array which is the second argument
+    // console.log("RUN USE EFFECT");
+    // console.log(name);
+  // }, [name]);   // [name] is a dependency array which is the second argument
                 // of the useEffect hook to control where the useEffect hook runs
 
   return (
@@ -36,8 +36,8 @@ const Home = () => {
       {/* pass the props in Home.js */}
       <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Xander')} title="Xander's blogs!" /> */}
-      <button onClick={() => setName('Wendy')}>Change Name</button>
-      <p>{name}</p>
+      {/* <button onClick={() => setName('Wendy')}>Change Name</button> */}
+      {/* <p>{name}</p> */}
     </div>
   );
 }
