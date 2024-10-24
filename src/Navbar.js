80efa28b7f7d3e80b6f22eb2dea954642948ae0e
wendy munyasi <wendymunyasi@gmail.com>
2024-10-24 16:43:13 +0300

    Add router links to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,19 @@
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
-  return ( 
+  return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-          <a href="/">Home</a>
-          <a href="/create" style={{  // inline styling 
+          <Link to="/">Home</Link>
+          <Link to="/create" style={{  // inline styling
             color: 'white', // style is an object of key-value pairs
             backgroundColor: '#f1356d', // hence the use of second {}
             borderRadius: '8px'
-          }} >New Blog</a>
+          }} >New Blog</Link>
       </div>
     </nav>
   );
 }
- 
+
 export default Navbar;
