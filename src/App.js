c357b83db71846b85faa9a5b63c373c1f932264c
wendy munyasi <wendymunyasi@gmail.com>
2024-10-24 15:41:47 +0300

    Add Create component/configure routes Home, Create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-          <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes> {/* This component that makes sure that only one route
+                      is rendered/ shows at any one time */}
+            <Route exact path="/" element={<Home />}>
+               {/* Home is the component that will be rendered */}
+            </Route>
+            <Route exact path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
