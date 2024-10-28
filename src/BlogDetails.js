b3faf905a7f5c5a87bc3715e0134fb54d8930964
wendy munyasi <wendymunyasi@gmail.com>
2024-10-28 19:26:52 +0300

    Add details of blog by reusing custom hook

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const {id} = useParams(); // useParams() grabs route params from route
   // destructure the parameter in the const
+  const {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 }
