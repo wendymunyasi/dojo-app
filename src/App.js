ee34a2f02f0ab9751a3625803b83fa1fdda3e500
wendy munyasi <wendymunyasi@gmail.com>
2024-10-28 22:01:31 +0300

    Add 404 page to blog

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './notFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             </Route>
             <Route exact path="/create" element={<Create />}></Route>
             <Route exact path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route exact path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
