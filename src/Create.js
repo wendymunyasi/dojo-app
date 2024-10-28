b02ed343db7156cada67e94dd46ff352b7934d10
wendy munyasi <wendymunyasi@gmail.com>
2024-10-28 20:13:43 +0300

    Add newBlog form/component with controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Trizah');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)} // e is event, this picks
+          // value from input and triggers the onChange function with setTitle
+          // thus updates the title
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Wendy">Wendy</option>
+          <option value="Trizah">Trizah</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
