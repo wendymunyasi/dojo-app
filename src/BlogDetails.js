3d50ece077c4ae276f247b790bce0dc0c59922bb
wendy munyasi <wendymunyasi@gmail.com>
2024-10-28 21:29:03 +0300

    Add delete blog button with onClick func

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const {id} = useParams(); // useParams() grabs route params from route
   // destructure the parameter in the const
   const {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useNavigate();
+
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history('/'); // navigate back to homepage
+    })
+  }
 
   return (
     <div className="blog-details">
@@ -15,6 +24,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete this blog</button>
         </article>
       )}
     </div>
